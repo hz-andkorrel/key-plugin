ae8b7ae30a8f794dd266d843fc9ae35e997057b6
Julien Nout <nout0001@hz.nl>
2025-11-24 12:15:51 +0100

    index.html volume mounted for dynamic displaying of html

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -10,14 +12,23 @@ import (
 func main() {
 	ctx := context.Background()
 
-	redis := redis.NewClient(&redis.Options{
+	rdb := redis.NewClient(&redis.Options{
 		Addr: "host.docker.internal:6379",
 		DB:   0,
 	})
 
-	subscription := redis.Subscribe(ctx, "hotel.events")
+	go func() {
+		subscription := rdb.Subscribe(ctx, "hotel.events")
+		for msg := range subscription.Channel() {
+			fmt.Printf("🔓 Unlocking door for: %s\n", msg.Payload)
+		}
+	}()
 
-	for msg := range subscription.Channel() {
-		fmt.Printf("🔓 Unlocking door for: %s\n", msg.Payload)
-	}
+	http.HandleFunc("/render", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	port := ":8080"
+	fmt.Printf("Server starting on port %s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
